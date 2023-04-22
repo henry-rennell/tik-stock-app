80243356ac8d05f1e1a2af7335a1bc8e29c80510
leonyu <13200666@qq.com>
2023-04-22 11:29:55 +1000

    sat morning

--- src/components/SmallStockCard.jsx
+++ src/components/SmallStockCard.jsx
@@ -1,13 +1,17 @@
 import "./SmallStockCard.css"
+import StockChart from "./chart"
 
 export default function SmallStockCard({ ticker, isGreen }) {
-
-    return (
-            <div className="small-stock-card">
-                <h3>{ticker.symbol}</h3>
-                <footer>
-                <span className="price">Price: {ticker.price}</span><span className={isGreen? "green" : "red"}>Change: {ticker.change}</span>
-                </footer>
-            </div>
-    )
+  return (
+    <div className="small-stock-card">
+      <h3>{ticker.symbol}</h3>
+      <StockChart name={ticker.symbol} />
+      <footer>
+        <span className="price">Price: {ticker.price}</span>
+        <span className={isGreen ? "green" : "red"}>
+          Change: {ticker.change}
+        </span>
+      </footer>
+    </div>
+  )
 }
